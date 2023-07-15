1d0db86892e74dc0bd4d2b368225edac00f330d5
naimur53 <naimurrhman53@gmail.com>
2023-07-16 02:23:39 +0600

    add uesr name on navbar

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../../redux/hook";
 import { userLoggedOut } from "../../redux/features/auth/authSlice";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 const NavBar = () => {
   const { user } = useAppSelector((state) => state.user);
   const dispatch = useAppDispatch();
@@ -15,24 +17,36 @@ const NavBar = () => {
           <Link to="/">
             <h2 className="text-xl font-black">Booker</h2>
           </Link>
-          <ul className="hidden md:flex items-center space-x-6">
-            <li className=" cursor-pointer">
-              <NavLink to="all-books">All Books</NavLink>
-            </li>
-            <li className="cursor-pointer">
-              <Link to="/wishlist">Wishlist</Link>
-            </li>
-            <li className="cursor-pointer">
-              <Link to="/add-new-book">Add Book</Link>
-            </li>
-            <li className="cursor-pointer">
-              {user.name ? (
-                <button onClick={handleLogout}>Logout</button>
+          <div className="relative group">
+            <div className="">
+              <FontAwesomeIcon className="m" icon={faBars} />
+            </div>
+            <ul className="group-hover:flex  absolute top-[20px] right-0 w-[200px] md:w-auto md:static hidden md:flex bg-blue-200 shadow md:shadow-none rounded-md py-2 md:rounded-none flex-col md:flex-row  items-center  gap-2 md:gap-6 ">
+              <li className=" cursor-pointer text-center">
+                <NavLink to="all-books">Books</NavLink>
+              </li>
+              <li className="cursor-pointer">
+                <Link to="/wishlist">Wishlist</Link>
+              </li>
+              <li className="cursor-pointer">
+                <Link to="/add-new-book">Add Book</Link>
+              </li>
+              {user.email ? (
+                <li>
+                  <span className="font-bold capitalize">{user.name}</span>
+                </li>
               ) : (
-                <Link to="/sign-in">Sign In</Link>
+                <></>
               )}
-            </li>
-          </ul>
+              <li className="cursor-pointer">
+                {user.name ? (
+                  <button onClick={handleLogout}>Logout</button>
+                ) : (
+                  <Link to="/sign-in">Sign In</Link>
+                )}
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     </div>
